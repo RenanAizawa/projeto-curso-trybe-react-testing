9c58912d9d164f2187304041370290e88cea2475
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-03-23 15:29:33 -0500

    requisito 2 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('Iniciando o projeto', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente About', () => {
+  it('este se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    expect(screen.getByRole('heading', { name: /About Pokédex/i })).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { history, getByAltText } = renderWithRouter(<App />);
+    history.push('/about');
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = getByAltText('Pokédex');
+    expect(image).toHaveAttribute('src', url);
+  });
+});
