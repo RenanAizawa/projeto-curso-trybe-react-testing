8935f4bdbf6b1b6c48fc44f69965e9570ebac8b2
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-03-27 22:03:23 -0500

    requisito 3 concluido, passou no stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente FavoritePokemons', () => {
+  it('verificar se a mensagem correta é rendenizada corretamente', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('favorites');
+    const mensagem = screen.getByText(/No favorite pokemon found/i);
+    expect(mensagem).toBeInTheDocument();
+  });
+});
